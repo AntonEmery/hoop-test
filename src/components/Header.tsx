e53c3374f1bc71a2e5ba64792827e35f0e5935b4
Anton Emery <antonemery@gmail.com>
2024-11-06 13:40:00 -0800

    Add main app container and header component
    This is just the main app black background and the header component
    that holds the Hoop logo and the user avatar

---
+++ src/components/Header.tsx
@@ -0,0 +1,18 @@
+import logoUrl from '../assets/hoop-logo.png';
+import avatarUrl from '../assets/avatar-web.jpg';
+
+
+const Header = () => {
+  return (
+    <header className="h-16 flex items-center justify-between px-4 py-4 bg-black text-white">
+        <img className="h-full w-auto" src={logoUrl} alt="Hoop Logo" />
+        <img
+          src={avatarUrl}
+          alt="User Avatar"
+          className="h-full w-auto rounded-full"
+        />
+    </header>
+  );
+};
+
+export default Header;
