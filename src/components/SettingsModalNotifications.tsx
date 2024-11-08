e5dce9917aefe493859a0a9ae9a827c8d36e61bb
Anton Emery <antonemery@gmail.com>
2024-11-07 16:51:17 -0800

    Add components for the Account and Notifications sections

---
+++ src/components/SettingsModalNotifications.tsx
@@ -0,0 +1,9 @@
+const SettingsModalNotifications = () => {
+  return (
+    <main className="flex-grow pt-8 pl-10 pr-8 rounded-lg bg-[var(--secondary-background)] border border-[var(--border-color)]">
+      <h2 className="text-lg font-semibold mb-4">Notifications</h2>
+    </main>
+  )
+}
+
+export default SettingsModalNotifications;
