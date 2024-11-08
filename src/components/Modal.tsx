de8ed5dfe61951b35f3a1ea129f89775ac5fefbf
Anton Emery <antonemery@gmail.com>
2024-11-07 16:49:14 -0800

    Fix styling on main parent Modal component to match comp

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -13,11 +13,11 @@ const Modal = ({ isOpen, onClose, hasCloseIcon = true, children }: ModalProps) =
 
   return ReactDOM.createPortal(
     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-      <div className="bg-[var(--color-light-black)] rounded-lg p-6 w-full max-w-md mx-4 relative shadow-lg">
+      <div className="border border-[var(--border-color)] rounded-lg p-6 w-[743px] h-[512px] mx-4 px-2 py-2 relative shadow-lg">
         {hasCloseIcon && (
         <button
           onClick={onClose}
-          className="absolute top-4 right-4 text-[var(--icon-color)] hover:text-[var(--icon-hover-color)]"
+          className="absolute top-9 right-6 font-bold text-2xl text-[var(--icon-color)] hover:text-[var(--icon-hover-color)]"
           aria-label="Close"
         >
           &times;
